f5ce428424cfb472096e518da9ba406d316c9eea
Florent Flament <contact@florentflament.com>
2018-08-12 12:01:31 +0200

    Update PF1 while computing shifts

--- src/fx_pixscroll_kernel.asm
+++ src/fx_pixscroll_kernel.asm
@@ -37,19 +37,6 @@
 	inx
 	ENDM
 
-
-; Load graphics into fxp_line registers
-; ptr must points towards the first element of the line to display
-; This macro uses a, x and y registers
-	MAC m_fxp_load_gfx
-	ldx #0
-	ldy #0
-.load_loop:
-	m_fxp_load_elmt
-	cpx #5 ; 5 registers to fill
-	bne .load_loop
-	ENDM
-
 ; rotate one line to the left
 	MAC m_fxp_rotate_line_left
 	lda fxp_line
@@ -70,27 +57,89 @@ fx_pixscroll_kernel SUBROUTINE
 .kern_loop:
 	sta WSYNC
 	lda fxp_line
+	sta fxp_pf0_buf
 	sta PF1
 	lda fxp_line + 1
 	sta GRP0
 	lda fxp_line + 2
 	sta GRP1
+	lda fxp_line + 3
+	sta fxp_pf3_buf
 
 	ldx #$0
 	ldy #$0
-	
-	m_fxp_load_gfx
-	m_fxp_rotate_line_left
-	m_fxp_rotate_line_left
-	m_fxp_rotate_line_left
-	m_fxp_rotate_line_left
+	m_fxp_load_elmt
+	lda fxp_pf3_buf
+	sta PF1
 
+	sta WSYNC
+	lda fxp_pf0_buf
+	sta PF1
+	m_fxp_load_elmt
+	m_fxp_load_elmt
+	lda fxp_pf3_buf
+	sta PF1
+
+	sta WSYNC
+	lda fxp_pf0_buf
+	sta PF1
+	m_fxp_load_elmt
+	m_fxp_load_elmt
+	lda fxp_pf3_buf
+	sta PF1
+
+	sta WSYNC
+	lda fxp_pf0_buf
+	sta PF1
+	m_fxp_rotate_line_left
+	lda fxp_pf3_buf
+	sta PF1
+
+	sta WSYNC
+	lda fxp_pf0_buf
+	sta PF1
+	m_fxp_rotate_line_left
+	lda fxp_pf3_buf
+	sta PF1
+
+	sta WSYNC
+	lda fxp_pf0_buf
+	sta PF1
+	m_fxp_rotate_line_left
+	lda fxp_pf3_buf
+	sta PF1
+
+	sta WSYNC
+	lda fxp_pf0_buf
+	sta PF1
+	m_fxp_rotate_line_left
+	lda fxp_pf3_buf
+	sta PF1
+
+	sta WSYNC
+	lda fxp_pf0_buf
+	sta PF1
  	m_add_to_pointer ptr, #1
-	dec tmp
-	bpl .kern_loop
+	REPEAT 4
+	nop
+	REPEND
+	lda fxp_pf3_buf
+	sta PF1
 
+	dec tmp
+	bmi .end
+	jmp .kern_loop
+
+.end:
+	sta WSYNC
 	lda #$0
 	sta PF1
 	sta GRP0
 	sta GRP1
+
+	ldx #$04
+.zero_fxp_line
+	sta fxp_line,x
+	dex
+	bpl .zero_fxp_line
 	rts
--- src/fx_pixscroll_variables.asm
+++ src/fx_pixscroll_variables.asm
@@ -1,2 +1,4 @@
 fxp_pix_base ds 2 ; Pointer towards the picture to display (base address)
 fxp_line     ds 5 ; registers to compute a line's graphics
+fxp_pf0_buf  ds 1 ; pf0 buffer
+fxp_pf3_buf  ds 1 ; pf3 buffer
