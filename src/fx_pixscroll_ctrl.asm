e145073c2e935fb6a242010a7c45989491665c9b
Florent Flament <contact@florentflament.com>
2018-08-13 16:11:25 +0200

    Change test gfx to Flush logo

--- src/fx_pixscroll_ctrl.asm
+++ src/fx_pixscroll_ctrl.asm
@@ -65,18 +65,31 @@ fx_pixscroll_vblank SUBROUTINE
 	; Initialize
 	m_copy_pointer fxp_pix_base, ptr
 	lda frame_cnt
-	REPEAT 3
-	lsr
-	REPEND
-	and #$03
-	beq .continue
+	and #$07
+	bne .no_shift
+
+	; Shift the picture
+	inc fxp_shift_i
+	lda fxp_shift_i
+	cmp #6 ; 6 columns picture
+	bne .no_shift
+	lda #0
+	sta fxp_shift_i
+
+.no_shift
+	; no rough repositionning if fxp_shift_i is null
+	lda fxp_shift_i
+	beq .no_rough
+
+	; Shift the pointer in the image
+	; i.e Move the picture by 8 bits
 	tax
 .rough_move:
 	m_add_to_pointer ptr, #30
 	dex
 	bne .rough_move
 
-.continue:
+.no_rough:
 	; Precompute the first line
 	ldx #$00
 	ldy #$00
@@ -102,35 +115,49 @@ fx_pixscroll_vblank SUBROUTINE
 	rts
 
 fxp_test_gfx:
-	dc.b $00, $9f, $00, $ff, $00, $ff, $00, $ff
-	dc.b $00, $9f, $00, $ff, $00, $ff, $00, $ff
-	dc.b $00, $9f, $00, $ff, $00, $ff, $00, $ff
-	dc.b $00, $9f, $00, $ff, $00, $ff
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00
-	dc.b $06, $ff, $00, $ff, $00, $ff, $00, $ff
-	dc.b $06, $ff, $00, $ff, $00, $ff, $00, $ff
-	dc.b $06, $ff, $00, $ff, $00, $ff, $00, $ff
-	dc.b $06, $ff, $00, $ff, $00, $ff
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00
-	dc.b $00, $9f, $00, $ff, $00, $ff, $00, $ff
-	dc.b $00, $9f, $00, $ff, $00, $ff, $00, $ff
-	dc.b $00, $9f, $00, $ff, $00, $ff, $00, $ff
-	dc.b $00, $9f, $00, $ff, $00, $ff
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00
-	dc.b $06, $ff, $00, $ff, $00, $ff, $00, $ff
-	dc.b $06, $ff, $00, $ff, $00, $ff, $00, $ff
-	dc.b $06, $ff, $00, $ff, $00, $ff, $00, $ff
-	dc.b $06, $ff, $00, $ff, $00, $ff
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00, $ff, $00
-	dc.b $ff, $00, $ff, $00, $ff, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $1f
+	dc.b $00, $00, $78, $fc, $e0, $e0, $f0, $f0
+	dc.b $e0, $e0, $e0, $60, $00, $00, $1f, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $00
+	dc.b $00, $00, $00, $1f, $00, $ff, $00, $00
+	dc.b $60, $e0, $e0, $e0, $e0, $e0, $e0, $e0
+	dc.b $fc, $78, $00, $00, $ff, $00, $1f, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $30
+	dc.b $00, $ff, $00, $ff, $00, $00, $64, $e4
+	dc.b $e4, $e4, $e4, $e4, $e4, $ec, $fc, $78
+	dc.b $00, $00, $ff, $00, $ff, $00, $30, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $e0
+	dc.b $00, $ff, $00, $00, $78, $fc, $e0, $e0
+	dc.b $f8, $7c, $04, $04, $fc, $78, $00, $00
+	dc.b $ff, $00, $e0, $00, $00, $00, $00, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $e0
+	dc.b $00, $00, $64, $e4, $e4, $e4, $fc, $fc
+	dc.b $e4, $e4, $e4, $64, $00, $00, $e0, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $00
+	dc.b $00, $00, $44, $cc, $00, $00, $fc, $78
+	dc.b $00, $00, $00, $00, $00, $00, $00, $00
+	dc.b $00, $00, $00, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $1f
+	dc.b $00, $00, $78, $fc, $e0, $e0, $f0, $f0
+	dc.b $e0, $e0, $e0, $60, $00, $00, $1f, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $00
+	dc.b $00, $00, $00, $1f, $00, $ff, $00, $00
+	dc.b $60, $e0, $e0, $e0, $e0, $e0, $e0, $e0
+	dc.b $fc, $78, $00, $00, $ff, $00, $1f, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $30
+	dc.b $00, $ff, $00, $ff, $00, $00, $64, $e4
+	dc.b $e4, $e4, $e4, $e4, $e4, $ec, $fc, $78
+	dc.b $00, $00, $ff, $00, $ff, $00, $30, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $e0
+	dc.b $00, $ff, $00, $00, $78, $fc, $e0, $e0
+	dc.b $f8, $7c, $04, $04, $fc, $78, $00, $00
+	dc.b $ff, $00, $e0, $00, $00, $00, $00, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $e0
+	dc.b $00, $00, $64, $e4, $e4, $e4, $fc, $fc
+	dc.b $e4, $e4, $e4, $64, $00, $00, $e0, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $00
+	dc.b $00, $00, $00, $00, $00, $00, $00, $00
+	dc.b $00, $00, $44, $cc, $00, $00, $fc, $78
+	dc.b $00, $00, $00, $00, $00, $00, $00, $00
+	dc.b $00, $00, $00, $00
